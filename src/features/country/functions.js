1fd7646f01abb967bf39df1443e7398d94e872c0
jeromeklam <jeromeklam@free.fr>
2020-04-21 12:10:57 +0200

    Receipts

--- src/features/country/functions.js
+++ src/features/country/functions.js
@@ -9,22 +9,24 @@ import { buildModel } from 'freejsonapi';
  */
 export function countryAsOptions(object) {
   let arr   = [];
-  let items = buildModel(
-    object,
-    'FreeFW_Country',
-  );
-  items.forEach((item) => {
-    arr.push({value: item.id, label: item.cnty_name});
-  });
-  arr.sort(function (a, b) {
-    if (a.label > b.label) {
-      return 1;
-    } else {
-      if (a.label < b.label) {
-        return -1;
-      } 
-    }
-    return 0;
-  });
+  if (object) {
+    let items = buildModel(
+      object,
+      'FreeFW_Country',
+    );
+    items.forEach((item) => {
+      arr.push({value: item.id, label: item.cnty_name});
+    });
+    arr.sort(function (a, b) {
+      if (a.label > b.label) {
+        return 1;
+      } else {
+        if (a.label < b.label) {
+          return -1;
+        } 
+      }
+      return 0;
+    });
+  }
   return arr;
 }
--- src/features/receipt/Create.js
+++ src/features/receipt/Create.js
@@ -3,37 +3,99 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from './redux/actions';
+import { withRouter } from 'react-router-dom';
+import { getJsonApi } from 'freejsonapi';
+import { propagateModel } from '../../common';
+import { CenteredLoading3Dots, createError, createSuccess } from '../ui';
+import Form from './Form';
 
 export class Create extends Component {
   static propTypes = {
     receipt: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired,
+    loader: PropTypes.bool,
+  };
+  static defaultprops = {
+    loader: true,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      recId: 0,
+      item: false,
+      loading: true,
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.actions.loadOne(this.state.recId).then(result => {
+      const item = this.props.receipt.loadOneItem;
+      this.setState({ item: item, loading: false });
+    });
+  }
+
+  onCancel(event) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.props.onClose();
+  }
+
+  onSubmit(datas = {}) {
+    // Conversion des données en objet pour le service web
+    let obj = getJsonApi(datas, 'FreeAsso_Receipt', this.state.recId);
+    this.props.actions
+      .createOne(obj)
+      .then(result => {
+        createSuccess();
+        this.props.actions.propagateModel('FreeAsso_Receipt', result);
+        this.props.onClose();
+      })
+      .catch(errors => {
+        createError();
+      });
+  }
+
   render() {
+    const { item }  = this.state;
     return (
-      <div className="receipt-create">
-        Page Content: receipt/Create
+      <div className="receipt-create global-card">
+       {!item ? (
+          <CenteredLoading3Dots show={this.props.loader} />
+        ) : (
+          <div>
+            {item && 
+              <Form 
+                item={item} 
+                errors={this.props.receipt.createOneError}
+                countries={this.props.country.items}
+                onSubmit={this.onSubmit} 
+                onCancel={this.onCancel} 
+                onClose={this.props.onClose}
+              />
+            }
+          </div>
+        )}
       </div>
     );
   }
 }
 
-/* istanbul ignore next */
 function mapStateToProps(state) {
   return {
     receipt: state.receipt,
+    country: state.country,
+    lang: state.lang,
   };
 }
 
-/* istanbul ignore next */
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators({ ...actions }, dispatch)
+    actions: bindActionCreators({ ...actions, propagateModel }, dispatch),
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Create);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Create));
